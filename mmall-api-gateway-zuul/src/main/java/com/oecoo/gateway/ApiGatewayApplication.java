301c9008ac52e8604d996feb4a7e11e1a28733f5
goofyww <akindofattitude@163.com>
2019-07-30 04:11:23 +0800

    zuul 路由转发 排除 自定义 cookie重置 基于bus总线动态路由

---
+++ mmall-api-gateway-zuul/src/main/java/com/oecoo/gateway/ApiGatewayApplication.java
@@ -0,0 +1,33 @@
+package com.oecoo.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.netflix.zuul.filters.ZuulProperties;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableZuulProxy
+public class ApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayApplication.class, args);
+    }
+
+    /**
+     * 动态路由
+     * 使用 git 实时刷新 线上配置信息，
+     * 避免了新增 Api时，重启服务
+     *
+     * @return
+     */
+    @ConfigurationProperties("zuul")
+    @RefreshScope
+    public ZuulProperties zuulProperties() {
+        return new ZuulProperties();
+    }
+
+}
